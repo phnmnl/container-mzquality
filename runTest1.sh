6e72190e55c61b4f3d73001075e718886eb28c51
Michael van Vliet <info@michaelvanvliet.nl>
2018-06-21 13:12:46 +0200

    bug fix, was using wrong data to test

--- runTest1.sh
+++ runTest1.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # run example dataset
-python /files/mzQuality/qcli.py blank_effect --mea-file=/files/mzQuality/data/combined.tsv --blank-effect-file=/tmp/be.tsv
+python /files/mzQuality/qcli.py blank_effect --mea-file=/tmp/combined.tsv --blank-effect-file=/tmp/be.tsv
 
 # compare output
-if ! cmp -s "/files/mzQuality/data/blank_effect.tsv" "/tmp/be.tsv"; then
+if ! cmp -s "/tmp/blank_effect.tsv" "/tmp/be.tsv"; then
 	echo "Blank effect files don't match!"
-	cat "/tmp/be.tsv"
 	cat /tmp/be.tsv
-	cat "/files/mzQuality/data/blank_effect.tsv"
-	cat /files/mzQuality/data/blank_effect.tsv
+	cat /tmp/blank_effect.tsv
 	exit 1
 fi
 
