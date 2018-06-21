dd81b0fb592410212716bb3ad1bee4192e04f722
Michael van Vliet <info@michaelvanvliet.nl>
2018-06-21 12:37:27 +0200

    Correct path to qcli.py

--- runTest1.sh
+++ runTest1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # run example dataset
-python /files/mzquality/qcli.py blank_effect --mea-file=/files/mzquality/data/combined.tsv --blank-effect-file=/files/mzquality/data/blank_effect_test.tsv
-python /files/mzquality/qcli.py rt_shifts --mea-file=/files/mzquality/data/combined.tsv --rt-shifts-file=/files/mzquality/data/rt_shifts_test.tsv
+python /files/mzQuality/qcli.py blank_effect --mea-file=/files/mzQuality/data/combined.tsv --blank-effect-file=/files/mzQuality/data/blank_effect_test.tsv
+python /files/mzQuality/qcli.py rt_shifts --mea-file=/files/mzQuality/data/combined.tsv --rt-shifts-file=/files/mzQuality/data/rt_shifts_test.tsv
 
 # compare output
-if ! cmp -s "/files/mzquality/data/blank_effect.tsv" "/files/mzquality/data/blank_effect_test.tsv"; then
+if ! cmp -s "/files/mzQuality/data/blank_effect.tsv" "/files/mzQuality/data/blank_effect_test.tsv"; then
 	echo "Blank effect files don't match!"
 	exit 1
 fi
 
-if ! cmp -s "/files/mzquality/data/rt_shifts.tsv" "/files/mzquality/data/rt_shifts_test.tsv"; then
+if ! cmp -s "/files/mzQuality/data/rt_shifts.tsv" "/files/mzQuality/data/rt_shifts_test.tsv"; then
 	echo "RT shifts files don't match!"
 	exit 1
 fi
